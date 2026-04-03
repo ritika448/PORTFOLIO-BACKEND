3dfaf83858cf3380e03d76083c90c60e3ad95a53
ritika bhangade <bhangaderitika80@gmail.com>
2026-04-03 14:56:30 +0530

    updated backend url

--- server.js
+++ server.js
@@ -26,11 +26,13 @@ app.post('/api/contact', async (req, res) => {
 
   // Use the email user from environment variables
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
+      pass: process.env.EMAIL_PASS
+    }
   });
 
   const mailOptions = {
